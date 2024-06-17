81d581dd329de5b0f3e0f75a9264b55f5fb7aad4
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-17 13:54:24 +0200

    js updated img for backgroudnd commit 4

---
+++ game.js
@@ -0,0 +1,29 @@
+const startButton = document.querySelector('.start-button');
+const gameScreen = document.getElementById('game-screen');
+const headerImage = document.querySelector('.header-image');
+const arrowImage = document.querySelector('.arrows-image');
+const paragraph = document.querySelector('p'); 
+const paragraph1 = document.querySelector('p1');
+const gardenImage = document.querySelector('.garden-image');
+const startPage = document.querySelector('.start-page');
+
+startButton.addEventListener('click', () => {
+    
+    startButton.style.display = 'none';
+    headerImage.style.display = 'none';
+    arrowImage.style.display = 'none';
+    paragraph.style.display = 'none';
+    paragraph1.style.display = 'none';
+    
+    gameScreen.style.display = 'flex';
+    gardenImage.style.display = 'block';
+    startPage.style.display = 'block';
+    
+    startGame();
+});
+
+function startGame() {
+    
+
+    console.log('game started');
+}
