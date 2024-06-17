90c674534b7faf679bd31b978261847171120c20
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-17 18:11:59 +0200

    7th commit player does not move correctly

--- game.js
+++ game.js
@@ -6,6 +6,12 @@ const paragraph = document.querySelector('p');
 const paragraph1 = document.querySelector('p1');
 const gardenImage = document.querySelector('.garden-image');
 const startPage = document.querySelector('.start-page');
+const playerImage = document.querySelector('.player-image');
+
+let playerPosition = {
+    x: 20,
+    y: 40
+};
 
 startButton.addEventListener('click', () => {
     
@@ -14,6 +20,9 @@ startButton.addEventListener('click', () => {
     arrowImage.style.display = 'none';
     paragraph.style.display = 'none';
     paragraph1.style.display = 'none';
+    if (paragraph1) {
+        paragraph1.style.display = 'none';
+    }
     
     gameScreen.style.display = 'flex';
     gardenImage.style.display = 'block';
@@ -24,9 +33,44 @@ startButton.addEventListener('click', () => {
 
 function startGame() {
     
+    document.addEventListener('keydown', handleKeyDown);
+  
+}
+function handleKeyDown(event) {
+    const speed = 10;
+    switch (event.key) {
+        case 'ArrowLeft':
+            movePlayer(-speed,0);
+            break;
+            case 'ArrowRight':
+                movePlayer(+speed, 0);
+                break;
+    }
+}
+function movePlayer(exxenX, exxenY) {
+    playerPosition.x += exxenX;
+    playerPosition.y += exxenY;
 
-    console.log('game started');   
+    const gameScreenLimit = gameScreen.getBoundingClientRect();
+    const playerLimit = playerImage.getBoundingClientRect();
+
+    const maxX = gameScreenLimit.width - playerLimit.width;
+    const maxY = gameScreenLimit.height - playerLimit.height;
+
+    if(playerPosition.x < 0) {
+        playerPosition = 0;
+    } else if (playerPosition.x > maxX) {
+        playerPosition.x = maxX;
+    }
+
+    if (playerPosition.y < 0) {
+        playerPosition.y = 0;
+    } else if (playerPosition.y > maxY) {
+        playerPosition.y = maxY;
+    }
+
+    playerImage.style.left = playerPosition.x + 'px';
+    playerImage.style.top = playerPosition.y + 'px';
 }
-function initializeGame() {
-    
-}
+
+
